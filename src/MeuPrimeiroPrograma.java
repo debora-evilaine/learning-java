8229d92302837dfb19e241a9cb351af097d9c77b
debora-evilaine <sousxdebora@gmail.com>
2024-02-27 15:54:40 -0300

    Initial commit with first java program

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,16 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args){
+        minhaInstrucaoDeImprimir();
+
+        System.out.println(2 + 2);
+        System.out.println(2 + 6);
+        System.out.println("ola texto");
+        System.out.println(10 / 3);
+
+    }
+
+    static void minhaInstrucaoDeImprimir(){
+        System.out.println("Estou chamando esse método!");
+    }
+}
