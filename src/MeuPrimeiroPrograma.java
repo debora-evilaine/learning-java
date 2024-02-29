babbb6e9a6f1d4d7b766d7633fb99c3286e8b77e
debora-evilaine <sousxdebora@gmail.com>
2024-02-29 12:41:55 -0300

    Aprendendo funções

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,43 +1,15 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        var login = "Olá";
-        var password = "orddlds";
-
-        /* TIPOS DE VARIÁVEIS:
-        Strings -> texto
-        Ints -> inteiros
-        Double -> Números fracionários
-        Boolean -> Verdadeiro ou Falso */
-
-        //TIPOS DE DADOS
-
-        String novoLogin = "Olá";
-        int number1 = 10;
-        double percent = 24.33;
-        boolean isDriver = true;
-
-        System.out.println(login);
-        System.out.println(password);
-        System.out.println(novoLogin);
-        System.out.println(number1);
-        System.out.println(percent);
-        System.out.println(isDriver);
-
-        number1 = 30;
-        System.out.println();
-        System.out.println(number1);
-        System.out.println(number1 - 5);
-
-        /*var name = "dkdkdkd";
-        name = 51;
-        Essas linhas de código retornam um erro,
-        pois a variável name é dita como String, mas depois
-        tentamos passar um valor numérico (nesse caso, um inteiro) a ela. */
-
-
-
-
+        sum(10, 5);
+        sum(5, 10);
     }
+    static void sum(int x, int y){
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+    }
+
 
 }
