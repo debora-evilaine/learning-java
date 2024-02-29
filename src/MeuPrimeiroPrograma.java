9590178383cb28de62ea532c49501a5b1450073d
debora-evilaine <sousxdebora@gmail.com>
2024-02-29 11:18:30 -0300

    Código da aula de programação dinâmica com variáveis

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,16 +1,43 @@
 public class MeuPrimeiroPrograma {
 
-    public static void main(String[] args){
-        minhaInstrucaoDeImprimir();
+    public static void main(String[] args) {
+        var login = "Olá";
+        var password = "orddlds";
 
-        System.out.println(2 + 2);
-        System.out.println(2 + 6);
-        System.out.println("ola texto");
-        System.out.println(10 / 3);
+        /* TIPOS DE VARIÁVEIS:
+        Strings -> texto
+        Ints -> inteiros
+        Double -> Números fracionários
+        Boolean -> Verdadeiro ou Falso */
+
+        //TIPOS DE DADOS
+
+        String novoLogin = "Olá";
+        int number1 = 10;
+        double percent = 24.33;
+        boolean isDriver = true;
+
+        System.out.println(login);
+        System.out.println(password);
+        System.out.println(novoLogin);
+        System.out.println(number1);
+        System.out.println(percent);
+        System.out.println(isDriver);
+
+        number1 = 30;
+        System.out.println();
+        System.out.println(number1);
+        System.out.println(number1 - 5);
+
+        /*var name = "dkdkdkd";
+        name = 51;
+        Essas linhas de código retornam um erro,
+        pois a variável name é dita como String, mas depois
+        tentamos passar um valor numérico (nesse caso, um inteiro) a ela. */
+
+
+
 
     }
 
-    static void minhaInstrucaoDeImprimir(){
-        System.out.println("Estou chamando esse método!");
-    }
 }
