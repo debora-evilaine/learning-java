5349f1c89fc99edecf64c354ca0178d21b3c595b
debora-evilaine <sousxdebora@gmail.com>
2024-03-04 13:44:04 -0300

    feat: Working with loops and Random class

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,13 +1,31 @@
+import java.util.Random;
+
 public class MeuPrimeiroPrograma {
 
-    public static void main(String[] args) {
-        /*System.out.println("Bem vinda de volta, " + args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
-        System.out.println(args[3]); */
+    public static void main(String[] args){
+        Random generate = new Random();
+
+        //Gera um número aleatório que pode ir até
+        //a casa do número 60
+
+        int i;
+        /* while (i < 6){
+            int number = generate.nextInt(60);
+            System.out.println("O número é " + number);
+            i++;
+            System.out.println("O valor de i é " + i);
+            System.out.println();
+        } */
+
+        for(i = 0; i < 6; i++){
+            int number = generate.nextInt(60);
+            System.out.println("O número é " + number);
+            System.out.println("O valor de i é " + i);
+            System.out.println();
+        }
+
+
+
 
     }
-
-
-
 }
