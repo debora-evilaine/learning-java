d7486e0b755e0ebdaccd69ff57bd9d55efa66fa4
debora-evilaine <sousxdebora@gmail.com>
2024-03-06 15:55:13 -0300

    Aprendendo sobre constantes (final)

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class MeuPrimeiroPrograma {
     public static void main(String[] args){
-        int x = 10;//Adição do x como variável global
-        System.out.println("Antes de ressign: " + x);
-        /* if (true){
-            int x = 10; /* Variável local a esse escopo; não é
-            acessável fora desse if
-        }
-        System.out.println(x); */
+        //VARIÁVEIS IMUTÁVEIS (CONSTANTES)
 
-        //CORRIGIDA:
+        int x = 14; //Variável mutável
 
-        if(true){
-            x = 20;
-        }
-        System.out.println("Após o reassign: " + x);
+       final double pi = 3.14159; /*A palavra reservada "final" define que
+        a variável "pi" é imutável!! */
+
+        pi = 2; /* Isso gera um erro de compilação, pois a variável pi não
+        pode ser modificada */
+
+
+
+
 
     }
 }
