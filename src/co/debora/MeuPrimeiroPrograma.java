457da0d9f84174be4be627b42d612989bbcc26ba
debora-evilaine <sousxdebora@gmail.com>
2024-03-12 15:19:30 -0300

    Aprendendo a manipular Strings

--- src/co/debora/MeuPrimeiroPrograma.java
+++ src/co/debora/MeuPrimeiroPrograma.java
@@ -1,21 +1,37 @@
 package co.debora;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
 
-        //Casting: conversão de tipos
+        String x = "Olá mundo, esse é o novo mundo!";
 
-        double dd = 10.10;
-        short ss = 32;
+        System.out.println(x.length()); /* exibe a quantidade de caracteres na String,
+        incluíndo espaços em branco e acentos */
 
-        Scanner scanner = new Scanner(System.in);
 
-        int z = (int) scanner.nextDouble();
+        System.out.println("x concatenada: " + x + " concatenada"); //Concatenar um String  outra
 
-        System.out.println(z);
+        System.out.println(x.contains("novo")); //Para saber se existe tal palavra na String
+
+        System.out.println(x.indexOf("mundo")); //Nos mostra em que índice da String aquela palavra começa
+
+        System.out.println(x.lastIndexOf("mundo")); //No mostra em que índice aquela palavra aparece pela última vez na String
+
+        System.out.println(x.toUpperCase()); //Faz com que a String fique em caixa alta
+
+        System.out.println(x.toLowerCase()); //Faz com que a String fique em caixa baixa
+
+        System.out.println(x.trim()); //Remove espaços desnecessários
+
+        System.out.println(x.substring(5)); //Pega a String a partir do index definido
+
+        System.out.println(x.substring(6, 10)); //A mesma coisa que o anterior, porém com um valor de início e outro de fim
+
+        System.out.println(x.equals(8)); //Compara a String com outro valor, e diz se são iguais ou não (true/false)
 
 
     }
